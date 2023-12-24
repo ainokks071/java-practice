9ab353f9ed32e202ec55ffc7f69804b4eb1830bf
ainokks071 <ainokks071@gmail.com>
2023-12-25 00:52:26 +0900

    exception handling

--- ch02/src/varex/ScannerEx1.java
+++ ch02/src/varex/ScannerEx1.java
@@ -1,21 +1,21 @@
-//package varex;
-//
-//import java.util.Scanner;
-//
-//public class ScannerEx1 {
-//
-//	public static void main(String[] args) {
-//		
-//		System.out.println("숫자(정수)를 입력하세요.");
-//
-//		Scanner scanner = new Scanner(System.in);
-//		//스캐너 객체 생성 
-//		String input = scanner.nextLine(); 
-//		//nexLine()메소드 : 입력대기 상태에 있다가, 사용자가 입력을 마치고 엔터키를 누르면, 입력한 내용이 문자열로 반환된다.
-//
-//		int num2 = Integer.parseInt(input);
-//		//parseInt()메소드 : 문자열 -> 숫자로 변환하여 반환된다.
-//		System.out.println("숫자 " + num2 + "을(를) 입력하셨습니다.");
-//	}
-//
-//}
+package varex;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("숫자(정수)를 입력하세요.");
+
+		Scanner scanner = new Scanner(System.in);
+		//스캐너 객체 생성
+		String input = scanner.nextLine();
+		//nexLine()메소드 : 입력대기 상태에 있다가, 사용자가 입력을 마치고 엔터키를 누르면, 입력한 내용이 문자열로 반환된다.
+
+		int num2 = Integer.parseInt(input);
+		//parseInt()메소드 : 문자열 -> 숫자로 변환하여 반환된다.
+		System.out.println("숫자 " + num2 + "을(를) 입력하셨습니다.");
+	}
+
+}
